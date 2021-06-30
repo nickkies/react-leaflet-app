0e37b13aee36a4472045d102ddb0d836e0fc4467
YoonSung Kim <opqudk@gmail.com>
2021-06-30 16:00:09 +0900

    map container style, geojson layer style

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import {GeoJSON, MapContainer} from 'react-leaflet';
 import mapData from '../data/countries.json';
 import 'leaflet/dist/leaflet.css';
+import './MyMap.css';
 
 class MyMap extends Component {
 	state = { }
@@ -10,18 +11,30 @@ class MyMap extends Component {
 		console.log(mapData);
 	}
 
+	countryStyle = {
+		fillColor: 'red',
+		fillOpacity: 0.8,
+		color: 'black',
+		weight: 2,
+		dashArray: 2,
+	};
+
 	render() {
 		return (
 			<div style={{width: '100vh'}}>
 				<h1 style={{textAlign: 'center'}}>My Map</h1>
-					<MapContainer
-						style={{height: "80vh"}}
-						zoom={2}
-						center={[20, 100]}
-						scrollWheelZoom={true}
-						attributionControl={false}>
-						<GeoJSON data={mapData.features} />
-					</MapContainer>
+				<MapContainer
+					style={{height: "80vh"}}
+					zoom={2}
+					center={[20, 100]}
+					scrollWheelZoom={true}
+					attributionControl={false}
+				>
+					<GeoJSON
+						data={mapData.features}
+						style={this.countryStyle}
+					/>
+				</MapContainer>
 			</div>
 		);
 	}
